deb5b17a05b6ef467aa8356cef845ab5722190e0
Guilherme Miranda <guilherme_de_oliveira_n1@hotmail.com>
2023-09-16 23:48:43 -0300

    create: base service for inheritance & user service

---
+++ src/app/api/base.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+export class BaseService {
+  protected readonly API = environment.apiURL;
+
+  constructor(protected httpClient: HttpClient) {}
+
+  protected create<T, D>(item: T, path: string, useCredentials: boolean) {
+    const { url, options } = this.getUrlAndOptions(path, useCredentials);
+    return this.httpClient.post<D>(url, item, options);
+  }
+
+  protected getOne<T>(id: string, path: string, useCredentials: boolean) {
+    const { url, options } = this.getUrlAndOptions(path, useCredentials, id);
+    return this.httpClient.get<T>(url, options);
+  }
+
+  protected getAll<T>(path: string, useCredentials: boolean) {
+    const { url, options } = this.getUrlAndOptions(path, useCredentials);
+    return this.httpClient.get<T>(url, options);
+  }
+
+  protected update<T, D>(id: string, item: T, path: string, useCredentials: boolean) {
+    const { url, options } = this.getUrlAndOptions(path, useCredentials, id);
+    return this.httpClient.put<D>(url, item, options);
+  }
+
+  protected delete<T>(id: string, path: string, useCredentials: boolean) {
+    const { url, options } = this.getUrlAndOptions(path, useCredentials, id);
+    return this.httpClient.delete<T>(url, options);
+  }
+
+  protected getUrlAndOptions(path: string, useCredentials: boolean, id?: string) {
+    const url = id ? `${this.API}/${path}/${id}` : `${this.API}/${path}`;
+    const options = useCredentials ? { withCredentials: true } : {};
+    return { url, options };
+  }
+}
---
+++ src/app/api/users/user.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { BaseService } from '../base.service';
+import { APIUser } from './user.interface';
+import { APIBooking } from '../bookings/booking.interface';
+import { APIResponse } from '../api-response';
+import { UserInput } from './user-input.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService extends BaseService {
+  constructor(httpClient: HttpClient) {
+    super(httpClient);
+  }
+
+  public createUser(item: UserInput): Observable<APIResponse<APIUser>> {
+    return this.create(item, 'users', false);
+  }
+
+  public getUser(id: string): Observable<APIUser> {
+    return this.getOne(id, 'users', true);
+  }
+
+  public getUsers(): Observable<APIUser[]> {
+    return this.getAll('users', false);
+  }
+
+  public getUserBookings(id: string): Observable<APIBooking[]> {
+    const { url, options } = this.getUrlAndOptions('users', true, id);
+    return this.httpClient.get<APIBooking[]>(url, options);
+  }
+
+  public updateUser(id: string, item: UserInput): Observable<APIResponse<APIUser>> {
+    return this.update(id, item, 'users', true);
+  }
+
+  public deleteUser(id: string): Observable<{ message: string }> {
+    return this.delete(id, 'users', true);
+  }
+}
